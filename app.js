645c84f25fb6757fde20495c785447c036ea33e7
Zhaoxin Wu <jeremywu1995@gmail.com>
2018-03-16 12:14:17 +0900

    modifying template

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+// var bustimetable = require('./routes/bustimetable');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,12 +21,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//In the Express.js app, the web root simply needs to be set using app.use(express.static('public'))
+//so that the application knows where to fetch the static files.
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/bustimetable', index);
 
 // catch 404 and forward to error handler
+/*
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -34,7 +40,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  //set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -42,5 +49,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+*/
+app.use(function(req, res, next) {
+  res.render('404page');
+});
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
-
+router.get('/bustimetable', function(req, res, next) {
+  res.render('bustimetable');
+});
+router.get('/discount', function(req, res, next) {
+  res.render('discount');
+});
+router.get('/messageboard', function(req, res, next) {
+  res.render('messageboard');
+});
+router.get('/trashcheck', function(req, res, next) {
+  res.render('trashcheck');
+});
+router.get('/txtimetable', function(req, res, next) {
+  res.render('txtimetable');
+});
 module.exports = router;
